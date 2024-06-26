5f2c484c250a39710c47b9cc5824be56864bf113
IvkinNikita <n.nikita.ivkin@gmail.com>
2024-06-26 17:16:55 +0300

    modified:   sso/cmd/sso/main.go

--- sso/cmd/sso/main.go
+++ sso/cmd/sso/main.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
+
 	"github.com/IvkinNikita/GoAuthorization/sso/sso/cmd/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "development"
+	envProd  = "production" // Добавлено, если это пропущено в оригинальном коде
 )
 
 func main() {
@@ -11,8 +20,36 @@ func main() {
 
 	fmt.Println(cfg)
 	// TODO: initilization logger
-
+	log := setupLogger(cfg.Env)
+	log.Info("string application",
+		slog.String("env", cfg.Env),
+		slog.Any("env", cfg),
+		slog.Int("port", cfg.GRPC.Port))
+	log.Debug("debug message")
+	log.Error("error mesage")
+	log.Warn("warn message")
 	// TODO: initilization apppp (app)
 
 	// TODO: start gRPC-server app
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+
 }
